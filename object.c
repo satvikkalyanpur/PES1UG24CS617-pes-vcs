cfa273c5a5253c21917617e8ade2baffc9b7e0cd
Satvik Kalyanpur <satvikkalyanpur@gmail.com>
2026-04-16 08:14:26 +0000

    feat: implement object buffer creation and hashing

--- object.c
+++ object.c
@@ -103,7 +103,14 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
 
     char header[64];
     int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len) + 1;
+size_t total_len = header_len + len;
+char *buffer = malloc(total_len);
 
+memcpy(buffer, header, header_len);
+memcpy(buffer + header_len, data, len);
+
+ObjectID id;
+compute_hash(buffer, total_len, &id);
     return -1;
 }
 
